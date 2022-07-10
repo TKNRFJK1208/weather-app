f6d8fd6876b8e7610b6437cad4ebca1dde6fa088
takanori <takanori@goopter.com>
2022-07-09 17:45:07 -0700

    added first stuly of main screen

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,17 @@
       <div class="search-box">
         <input type="text" class="search-bar" placeholder="Search...">
       </div>
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Northampton, UK</div>
+          <div class="date">Monday 20 January 2020</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">9°c</div>
+          <div class="weather">Rain</div>
+        </div>
+      </div>
     </main>
   </div>
 </template>
